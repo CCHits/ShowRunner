9af5c726ae5e31f78654dac83ef6d2dbd2d42b21
Jon Spriggs <jon@sprig.gs>
2017-12-21 08:50:22 +0000

    Tweaks to support using SFTP and to enable debug

--- run_setup.sh
+++ run_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get dist-upgrade -y
-sudo apt-get install -y festival festlex-cmu festlex-poslex festlex-oald libestools1.2 unzip mp4v2-utils vorbis-tools eyed3 git sox libsox-fmt-all libav-tools php5-cli php5-curl sendemail libio-socket-ssl-perl libnet-ssleay-perl
+sudo apt-get install -y festival festlex-cmu festlex-poslex festlex-oald libestools1.2 unzip mp4v2-utils vorbis-tools eyed3 git sox libsox-fmt-all libav-tools php5-cli php5-curl sendemail libio-socket-ssl-perl libnet-ssleay-perl sshpass
 if [ ! -d /usr/share/festival/voices/english/cmu_us_clb_arctic_clunits ] ; then
   rm -Rf /var/cmu_voices
   mkdir /var/cmu_voices
